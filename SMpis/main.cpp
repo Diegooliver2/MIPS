dd1dc9783f255055cc4c24359ab640a3decb49c0
Diegooliver2 <42583983+Diegooliver2@users.noreply.github.com>
2019-11-21 11:03:54 -0300

    Add files via upload

---
+++ SMpis/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class Mips {
+private:
+    long int s[7];
+    long int t[9];
+    long int zero;
+    long int pc;
+
+public:
+    void ADDI(){}
+    void ADD(){}
+    void SUB(){}
+
+    // OU
+
+    void ULA(){}
+};
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
